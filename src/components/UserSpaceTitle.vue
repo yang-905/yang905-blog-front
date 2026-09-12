763dff69c6d3d5c4e5d3df60356b8c05d4828536
yang-905 <yangguangpu905@outlook.com>
2026-09-12 10:55:39 +0800

    页面升级

--- src/components/UserSpaceTitle.vue
+++ src/components/UserSpaceTitle.vue
@@ -1,23 +1,20 @@
 <template>
-    <div class="h-[300px] flex flex-col justify-end bg-cover bg-center"
-     :style="{ backgroundImage: 'url(' + titleimageUrl + ')' }">
-        <div class="h-[100px] w-[1000px] mx-auto mb-[10px]
-        grid grid-cols-[auto_1fr_auto] gap-[10px]">
-            
-            <div class="h-[100px] w-[100px] bg-zinc-400 rounded-[50%] overflow-hidden">
-                <!-- <i class="el-icon-user-solid text-[100px] text-zinc-300"></i> -->
-                <div class="h-full w-full bg-cover bg-center"
-                :style="{ backgroundImage: 'url(' + headimageUrl + ')' }"> </div>
-            </div>
-            <div class="my-auto text-white">
-                <i class="text-6xl">{{userInfo.username}}</i>
-                <br>
-                <i>{{userInfo.signature}}</i>
-            </div>
-            <div class="mt-auto text-white">
-                <i class="text-6xl mr-3">573</i>
-                <i class="el-icon-star-on text-[7vh] text-yellow-300"></i>
-            </div>
+    <div class="h-[100px] w-full
+    grid grid-cols-[auto_1fr_auto] gap-[10px]">
+        
+        <div class="h-[100px] w-[100px] bg-zinc-400 rounded-[50%] overflow-hidden">
+            <!-- <i class="el-icon-user-solid text-[100px] text-zinc-300"></i> -->
+            <div class="h-full w-full bg-cover bg-center"
+            :style="{ backgroundImage: 'url(' + headimageUrl + ')' }"> </div>
+        </div>
+        <div class="my-auto text-black">
+            <i class="text-6xl">{{userInfo.username}}</i>
+            <br>
+            <i>{{userInfo.signature}}</i>
+        </div>
+        <div class="mt-auto text-black">
+            <i class="text-6xl mr-3">573</i>
+            <i class="el-icon-star-on text-[7vh] text-yellow-300"></i>
         </div>
     </div>
 </template>
@@ -38,11 +35,8 @@ export default {
     computed: {
         headimageUrl() {
             return `http://localhost:3000/imagems/take?filename=${this.userInfo.headimageFilename}`;
-        },
-        titleimageUrl() {
-            return `http://localhost:3000/imagems/take?filename=${this.userInfo.titleimageFilename}`;
         }
-  }
+    }
 }
 </script>
 
--- src/index.vue
+++ src/index.vue
@@ -1,30 +1,39 @@
 <template>
   <div class="min-w-[1000px] min-h-[100vh] max-w-[2100px] mx-auto grid grid-rows-[auto_auto_1fr] bg-zinc-100">
-    <div class="bg-[#545c64]">
-      <div class="w-[1000px] mx-auto grid grid-cols-[auto_1fr]">
-        <div class="h-[100%] w-[60px] bg-[url('/public/905logo.png')] bg-contain bg-center bg-no-repeat">
+
+    
+    <div class="bg-[#545c64] w-full fixed top-0 left-0 z-20">
+        <div class="mx-[20px] grid grid-cols-[auto_1fr]">
+            <div class="h-full w-[60px] bg-[url('/public/905logo.png')] bg-contain bg-center bg-no-repeat">
+            </div>
+            <NavMenu />
         </div>
-        <NavMenu />
-      </div>
-    </div>
-    <div class="bg-white">
-
-      <UserSpaceTitle :userInfo="userInfo"/>
-
     </div>
 
-    <div class="grid grid-cols-[2fr_3fr] gap-[10px] w-[1000px] h-auto mx-auto mt-[20px]">
-      
-      <div class="" >
-        <div class="w-[100%] h-[100%] ">
-            <MarkdownRenderer :content="userInfo.articleContent" />
+    <div class="h-[100vh] w-[100vw] fixed top-0 left-0 bg-cover bg-center z-0"
+    :style="{ backgroundImage: 'url(' + titleimageUrl + ')' }">
+    </div>
+
+    
+    
+
+    <div class="w-[1500px] mx-auto z-10 mt-[60vh] bg-[rgba(255,255,255,0.3)] p-[40px]">
+        <div class="mb-[40px]">
+            <UserSpaceTitle :userInfo="userInfo"/>
         </div>
-      </div>
+        <div class="grid grid-cols-[2fr_3fr] gap-[10px] h-auto">
+            <div class="" >
+                <div class="w-[100%] h-[100%] ">
+                    <MarkdownRenderer :content="userInfo.articleContent" />
+                </div>
+            </div>
 
-      <div class="">
-        <InfiniteScroll />
-      </div>
+            <div class="">
+                <InfiniteScroll />
+            </div>
+        </div>
     </div>
+    
 
   </div>
 </template>
@@ -55,6 +64,9 @@ export default{
         }
     },
     computed: {
+        titleimageUrl() {
+            return `http://localhost:3000/imagems/take?filename=${this.userInfo.titleimageFilename}`;
+        }
     },
     mounted(){
         this.fetchUserInfo();
